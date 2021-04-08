3e1cd37ede71e5f348141ace0cbfb5bfcfce0def
苏剑林(Jianlin Su) <bojone@spaces.ac.cn>
2021-04-08 12:10:14 +0800

    修复tf.keras下的错误

--- gpt.py
+++ gpt.py
@@ -88,7 +88,7 @@ class CrossEntropy(Loss):
         return loss
 
 
-class PtuningEmbedding(Embedding):
+class PturningEmbedding(Embedding):
     """新定义Embedding层，只优化部分Token
     """
     def call(self, inputs, mode='embedding'):
@@ -97,7 +97,9 @@ class PtuningEmbedding(Embedding):
         mask = np.zeros((K.int_shape(embeddings)[0], 1))
         mask[1:9] += 1  # 只优化id为1～8的token
         self.embeddings = embeddings * mask + embeddings_sg * (1 - mask)
-        return super(PtuningEmbedding, self).call(inputs, mode)
+        outputs = super(PturningEmbedding, self).call(inputs, mode)
+        self.embeddings = embeddings
+        return outputs
 
 
 class PtuningBERT(BERT):
